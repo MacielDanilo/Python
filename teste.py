c3c36e559f0b04919f5b469386f82a8277ac9afd
MacielDanilo <macieldanilo29@gmail.com>
2023-10-03 20:55:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,14 @@
 nome = input ('Qual é o seu Nome? ')
-idade = input ('Quantos anos voce tem? ')
-peso = input ('Quanto voce pesa? ')
-print (nome,idade,peso)
+print ('Olá',nome, '! Prazer em te conhecer!')
+
+dia = input ('Qual o dia do seu nascimento? ')
+mes = input ('E qual o mes? ')
+ano = input ('Qual o ano? ')
+print ('Então Voce nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
+
+print('Vamos fazer uma soma! ')
+n1 = input ('Primeiro numero: ')
+n2 = input ('Segundo numero: ')
+n3 = n1+n2
+print ('A soma dos numeros',n1,'e',n2, 'é igual a',n3)
+
